c2c919459ee40d4b1732938929678b55403c0c15
dima-kulikov <skaizy44@gmail.com>
2021-09-02 17:09:26 +0300

    1
    1

--- #src/js/alert.js
+++
@@ -1,8 +0,0 @@
-const videos = document.querySelectorAll(".video");
-
-
-for (const video of videos) {
-    video.addEventListener('mouseover', function() { video.play() }, false);
-    video.addEventListener('mouseout', function() { video.pause() }, false);
-}
-
