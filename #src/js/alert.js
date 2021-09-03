1e1380da89b48f91e9f0a0d0267d9c17cc733869
dima-kulikov <skaizy44@gmail.com>
2021-09-03 18:11:42 +0300

    focus
    focus

---
+++ #src/js/alert.js
@@ -0,0 +1,24 @@
+// document.getElementById('pp').onclick = function() {
+//     document.getElementById('dd').classList.toggle('dima');
+//   }
+
+//   document.getElementById('pp1').onclick = function() {
+//     document.getElementById('dd1').classList.toggle('dima');
+//   }  
+
+//-----------------------
+// const accordeons = document.querySelectorAll('.dl-info-tabs-item__title');
+// const replacementIcon = document.querySelectorAll('.dl-section__open-btn');
+
+// function headerClick () {
+//     console.log(this);
+//     console.log(this.parentElement);
+//     this.parentElement.classList.toggle('dl-open');
+//     this.parentElement.querySelector('.dl-section__open-btn').classList.toggle('dl-section__open-btn__del-after');
+//     this.preventDefault()
+// };
+// accordeons.forEach(el => {
+//     el.addEventListener('click', headerClick);
+// })
+    
+//------
