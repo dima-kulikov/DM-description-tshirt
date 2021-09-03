01dd88912a1ec5c468649516497a2bfc902ff168
dima-kulikov <skaizy44@gmail.com>
2021-09-03 18:49:31 +0300

    js
    js

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,35 @@
-// document.getElementById('pp').onclick = function() {
-//     document.getElementById('dd').classList.toggle('dima');
-//   }
 
-//   document.getElementById('pp1').onclick = function() {
-//     document.getElementById('dd1').classList.toggle('dima');
-//   }  
 
 //-----------------------
-// const accordeons = document.querySelectorAll('.dl-info-tabs-item__title');
-// const replacementIcon = document.querySelectorAll('.dl-section__open-btn');
+// const circleEvent = document.querySelectorAll('.circle');
+
 
 // function headerClick () {
 //     console.log(this);
 //     console.log(this.parentElement);
-//     this.parentElement.classList.toggle('dl-open');
-//     this.parentElement.querySelector('.dl-section__open-btn').classList.toggle('dl-section__open-btn__del-after');
+//     this.parentElement.classList.toggle('circle-open');
 //     this.preventDefault()
 // };
-// accordeons.forEach(el => {
+// circleEvent.forEach(el => {
 //     el.addEventListener('click', headerClick);
 // })
     
 //------
+
+//-----------------------
+const circleEvent = document.querySelectorAll('.circle');
+
+function openDescription () {
+    console.log(this);
+    console.log(this.element);
+    this.classList.toggle('circle-open');
+    this.preventDefault()
+};
+circleEvent.forEach(el => {
+    el.addEventListener('click', openDescription);
+})
+    
+//----
 
 
 
