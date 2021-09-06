e6bcbeefa4ad76a8a60d54064187edcedd898e19
dima-kulikov <skaizy44@gmail.com>
2021-09-06 22:21:55 +0300

    111
    111

--- #src/js/alert.js
+++ #src/js/alert.js
@@ -18,15 +18,40 @@
 
 //-----------------------
 const circleEvent = document.querySelectorAll('.circle');
+const container = document.querySelector('.description')
+const open = document.querySelector('.circle-open')
+
+
+document.addEventListener('click', function(e) {
+	const items = document.querySelectorAll('.circle');
+	const target = e.target;
+  Array.from(items).forEach(item => {
+  	item.classList.remove('circle-open')
+  });
+  target.classList.add('circle-open');
+});
+
+
 
 function openDescription () {
     console.log(this);
-    console.log(this.element);
     this.classList.toggle('circle-open');
-    this.preventDefault()
+
+
+//   document.onclick = function(e){
+//    if ( this.classList != 'circle-open' ) {
+//       container.style.display = 'none';
+//       }     
+// };
+
+
 };
+
 circleEvent.forEach(el => {
     el.addEventListener('click', openDescription);
 })
     
 //----
+
+// const material = document.querySelector('.section-size')
+
