a5325848823f24535d0d04e850d8d15d7008cb61
dima-kulikov <skaizy44@gmail.com>
2021-09-07 18:14:07 +0300

    07/09-18
    07/09-18

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,44 +1,52 @@
-
-
-//-----------------------
-// const circleEvent = document.querySelectorAll('.circle');
-
-
-// function headerClick () {
-//     console.log(this);
-//     console.log(this.parentElement);
-//     this.parentElement.classList.toggle('circle-open');
-//     this.preventDefault()
-// };
-// circleEvent.forEach(el => {
-//     el.addEventListener('click', headerClick);
-// })
-    
-//------
-
 //-----------------------
 const circleEvent = document.querySelectorAll('.circle');
-const container = document.querySelector('.description')
-const open = document.querySelector('.circle-open')
+const container = document.querySelector('.description');
+const open = document.querySelector('.circle-open');
+const blockPoint = document.querySelector('.block-point');
 
+const isTouch = () => {
+  return !!('ontouchstart' in window || (navigator.msMaxTouchPoints > 0) || navigator.maxTouchPoints)
+}
 
-document.addEventListener('click', function(e) {
-	const items = document.querySelectorAll('.circle');
-	const { target } = e;
-
-  console.log(target);
-  
+const handler = (e) => {
+  const items = document.querySelectorAll('.circle');
+	const { target, type } = e;
+ 
   if (target.classList.contains('circle')) {
-    target.classList.toggle('circle-open');
+    items.forEach(item => {
+      if (!item.isEqualNode(target)) item.classList.remove('circle-open')
+    });
+    if(type === 'click') {
+      target.classList.toggle('circle-open');
+    } else if(type === 'mouseleave') {
+      target.classList.remove('circle-open');
+    } else {
+      target.classList.add('circle-open');
+    }
   } else {
     items.forEach(item => {
       item.classList.remove('circle-open')
     });
   }
-});
+}
 
+if (isTouch()) {
+  document.addEventListener('click', handler);
+} else {
+  circleEvent.forEach(item => {
+    item.addEventListener('mouseenter', handler);
+    item.addEventListener('mouseleave', handler);
+  })
+}
 
+const pp = () =>{
+  circleEvent[0].classList.add('circle-open');
+  // alert('374723')
+  // blockPoint.preventDefault();
+};
 
+blockPoint.addEventListener('mouseleave', pp);
+// document.getElementById("client").reset();
 
 
 
